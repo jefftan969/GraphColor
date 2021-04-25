c8c387ec181a44d8c6a3096a61cfac6c80745d2b
Sarah Pethani <spethani@andrew.cmu.edu>
2021-04-25 00:57:28 -0400

    sequential implementations of baseline greedy and jones plassman

--- jp.cpp
+++ jp.cpp
@@ -1,7 +1,85 @@
 #include "graph.h"
 
-int main() {
-    Graph graph = Graph("graphs/ex0");
-    graph.print();
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>
+#include <omp.h>
+
+std::vector<int> jpColoring(Graph& graph) {
+    int numVertices = graph.getNumVertices();
+    int currentColor = 0;
+
+    // Create the vector in which the final colors will be stored
+    // Element at index i will be the color of vertex i
+    std::vector<int> coloring(numVertices, -1);
+
+    // Keep a worklist, the set of vertices that need to be considered
+    std::vector<int> W;
+    for(int i = 0; i < numVertices; i++) {
+        W.push_back(i);
+    }
+
+    while(!W.empty()) {
+        // Assign random weights to each vertex
+        std::vector<int> weights(numVertices);
+        for (int i = 0; i < numVertices; i++) {
+            weights.at(i) = rand();
+        }
+
+        // Initialize the independent set of vertices
+        std::vector<int> S(0);
+
+        // Add vertices to the independent set
+        for (int j = 0; j < (int) W.size(); j++) {
+            int vertex = W.at(j);
+            bool flag = true;
+            const std::vector<int>& neighbors = graph.getNeighbors(vertex);
+            for (int j = 0; j < (int) neighbors.size(); j++) {
+                int neighbor = neighbors.at(j);
+                if (weights.at(vertex) <= weights.at(neighbor)) {
+                    flag = false;
+                }
+            }
+
+            if (flag) {
+                S.push_back(vertex);
+            }
+        }
+
+        // Color the independent set
+        for (int j = 0; j < (int) S.size(); j++) {
+            int vertex = S.at(j);
+            coloring.at(vertex) = currentColor;
+        }
+
+        // Update the working set and color
+        if (!S.empty()) {
+            std::vector<int> newW;
+            std::set_difference(W.begin(), W.end(), S.begin(), S.end(), 
+                                std::inserter(newW, newW.end()));
+            W = newW;
+            currentColor++;
+        }
+    }
+    return coloring;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " [in_filename]\n";
+        exit(0);
+    }
+
+    const std::string in_filename = argv[1];
+    Graph graph = Graph(in_filename);
+    
+    std::vector<int> coloring = jpColoring(graph);
+    bool validColoring = checkColoring(graph, coloring);
+
+    if (validColoring) printColoring(coloring);
+    else std::cout << "Invalid coloring\n";
+
     return 0;
 }
--- sequential.cpp
+++ sequential.cpp
@@ -1,7 +1,56 @@
 #include "graph.h"
 
-int main() {
-    Graph graph = Graph("graphs/ex0");
-    graph.print();
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<int> sequentialColoring(Graph& graph) {
+    int numVertices = graph.getNumVertices();
+
+    // Create the vector in which the final colors will be stored
+    // Element at index i will be the color of vertex i
+    std::vector<int> coloring(numVertices);
+
+    // Create vector which will keep track of neighbors' colors
+    std::vector<int> usedColors(numVertices);
+
+    // Loop through the vertices to determine color
+    for (int vertex = 0; vertex < numVertices; vertex++) {
+
+        // Loop through neighbors to keep track of their colors
+        const std::vector<int>& neighbors = graph.getNeighbors(vertex);
+
+        for (int j = 0; j < (int) neighbors.size(); j++) {
+            int neighbor = neighbors.at(j);
+            usedColors.at(coloring.at(neighbor)) = vertex;
+        }
+
+        // Assign the minimum color which is not shared by neighbors
+        for (int i = 0; i < numVertices; i++) {
+            if (usedColors.at(i) != vertex) {
+                coloring.at(vertex) = i;
+                break;
+            }
+        }
+    }
+
+    return coloring;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " [in_filename]\n";
+        exit(0);
+    }
+
+    const std::string in_filename = argv[1];
+    Graph graph = Graph(in_filename);
+    
+    std::vector<int> coloring = sequentialColoring(graph);
+    bool validColoring = checkColoring(graph, coloring);
+
+    if (validColoring) printColoring(coloring);
+    else std::cout << "Invalid coloring\n";
+
     return 0;
 }
