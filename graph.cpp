1f1d8710b52c44485cbd7a0201622801a3f5bdd5
Jeff Tan <jefftan@andrew.cmu.edu>
2021-05-09 14:31:27 -0400

    parse dimacs binary format

--- graph.cpp
+++ graph.cpp
@@ -29,17 +29,23 @@ Graph::Graph(const std::string &filename) :
     if(filename.substr(filename.length() - 4) == ".col") {
         // Interpret graph as DIMACS standard format
         parseDimacs(file);
-    } else if(filename.substr(filename.length() - 4) == ".col.b") {
+    } else if(filename.substr(filename.length() - 6) == ".col.b") {
         // Interpret graph as DIMACS binary format
         parseDimacsBinary(file);
-    } else {
+    } else if(filename.substr(filename.length() - 4) == ".txt") {
         // Interpret graph as edge list format
         parseEdgeList(file);
+    } else if(filename.substr(filename.length() - 4) == ".mtx") {
+        // Interpret graph as Matrix Market format
+        parseMatrixMarket(file);
+    } else {
+        std::cerr << "Error: File '" << filename << "' has an unknown format\n";
+        exit(-1);
     }
 }
 
 /**
- * @brief Parse file as DIMACS standard format graph representation
+ * @brief Parse graph representation as DIMACS edge list format
  * @param[in] file Input file
  */
 void Graph::parseDimacs(std::istream &file) {
@@ -50,12 +56,14 @@ void Graph::parseDimacs(std::istream &file) {
         ss >> linetype;
 
         if(linetype == 'e') {
+            // Read edge, note that DIMACS edge list format is 1-indexed
             int v1, v2;
             ss >> v1 >> v2;
-            graph_.at(v1-1).push_back(v2-1); // DIMACS edge list format is 1-indexed
+            graph_.at(v1-1).push_back(v2-1);
             graph_.at(v2-1).push_back(v1-1);
 
         } else if(linetype == 'p') {
+            // Read number of vertices
             std::string filetype;
             ss >> filetype;
             if(filetype != "edge") {
@@ -70,15 +78,62 @@ void Graph::parseDimacs(std::istream &file) {
 }
 
 /**
- * @brief Parse file as DIMACS binary format graph representation
+ * @brief Parse graph representation as DIMACS binary format
  * @param[in] file Input file
  */
 void Graph::parseDimacsBinary(std::istream &file) {
+    std::string line;
 
+    // Read preamble
+    std::getline(file, line);
+    while(std::getline(file, line)) {
+        char linetype;
+        std::stringstream ss(line);
+        ss >> linetype;
+
+        if(linetype == 'p') {
+            // Read number of vertices
+            std::string filetype;
+            ss >> filetype;
+            if(filetype != "edge") {
+                std::cerr << "Error: File is not in DIMACS binary format\n";
+                exit(-1);
+            }
+
+            ss >> numVertices_;
+            graph_.resize(numVertices_);
+            break;
+        }
+    }
+
+    // Read rows of adjacency matrix, and add edges to graph data structure
+    char *adjacencyRow = new char[numVertices_/8 + 1];
+    for(int i = 0; i < numVertices_; i++) {
+        file.read(adjacencyRow, i/8 + 1);
+
+        for(int j = 0; j <= i; j++) {
+            int bit = 0x07 - (j & 0x07);
+            int byte = j >> 3;
+            char mask = 0x01 << bit;
+
+            if((adjacencyRow[byte] & mask) == mask) {
+                graph_.at(i).push_back(j);
+                graph_.at(j).push_back(i);
+            }
+        }
+    }
 }
 
 /**
- * @brief Parse file as edge list graph representation
+ * @brief Parse graph representation as Matrix Market adjacency matrix format
+ * @param[in] file Input file
+ */
+void Graph::parseMatrixMarket(std::istream &file) {
+    // TODO implement matrix market parsing
+}
+
+/**
+ * @brief Parse graph representation as custom edge list format
  * @param[in] file Input file
  */
 void Graph::parseEdgeList(std::istream &file) {
