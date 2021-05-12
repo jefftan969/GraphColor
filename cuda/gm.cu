435f00aa811c71941a4a95ad5eca7b3026b592f8
Jeff Tan <jefftan@andrew.cmu.edu>
2021-05-12 11:41:41 -0400

    clean up cuda gm implementation

--- cuda/gm.cu
+++ cuda/gm.cu
@@ -9,7 +9,7 @@
 #include "graph.h"
 
 // NOTE: This code currently infinite-loops if the number of colors is greater than COLOR_MASK_SIZE.
-// These values are assigned due to constraints on the number of registers available in CUDA
+// These values are chosen due to the limited number of registers available in CUDA
 #define COLOR_MASK_SIZE 500
 #define BLOCK_SIZE 128
 
@@ -28,44 +28,69 @@ inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort=
 #define cudaCheckError(ans) ans
 #endif
 
-__global__ void kernelSpeculate(int numVertices, int numEdges, int blockSize,
-        const int *vertices, const int *neighbors, int *worklist, int *coloring) {
-    int v = blockIdx.x * blockSize + threadIdx.x;
+// Stores all data structures allocated on CUDA device
+struct cudaContext {
+    int numVertices;
+    int numEdges;
+    int blockSize;
+    int *vertices;  // Length numVertices+1 - List of graph vertices, in CSR representation
+    int *neighbors; // Length 2*numEdges    - List of vertex neighbors, in CSR representation
+    int *worklist;  // Length numVertices   - Boolean array indicating whether each vertex is in worklist
+    int *coloring;  // Length numVertices   - Integer array indicating color of each vertex
+    int *worklistEmptyFlag; // Boolean flag indicating whether the worklist is empty
+};
+
+/**
+ * @brief Speculatively color vertices in parallel, allowing conflicts which will be resolved later
+ * @param[in] context All data structures allocated on CUDA device
+ */
+__global__ void kernelSpeculate(struct cudaContext context) {
+    int v = blockIdx.x * BLOCK_SIZE + threadIdx.x;
     int colorMask[COLOR_MASK_SIZE];
 
+    int numVertices = context.numVertices;
+    const int *vertices = context.vertices;
+    const int *neighbors = context.neighbors;
+    int *worklist = context.worklist;
+    int *coloring = context.coloring;
+
     if(v < numVertices && worklist[v]) {
-//        printf("%d in if\n", v);
-        // Determine which colors are permissible for each vertex
-        for(int j = vertices[v]; j < vertices[v+1]; j++) {
-            int w = neighbors[j];
-//            printf("%d in for, neighbor %d, coloring[w]=%d\n", v, w, coloring[w]);
+        // Determine colors of all vertex neighbors
+        for(int i = vertices[v]; i < vertices[v+1]; i++) {
+            int w = neighbors[i];
             colorMask[coloring[w]] = v;
         }
-//        printf("%d colorMask[0]=%d\n", v, colorMask[0]);
-        for(int i = 0; i < COLOR_MASK_SIZE && i < numVertices; i++) {
-//            printf("%d in second for\n", v);
+        // Color each vertex with the minimal acceptable color
+        for(int i = 0; i < COLOR_MASK_SIZE; i++) {
             if(colorMask[i] != v) {
-//                printf("%d in setting coloring with i=%d\n", v, i);
                 coloring[v] = i;
-//                printf("%d Coloring: %d %d %d %d %d\n", v, coloring[0], coloring[1], coloring[2], coloring[3], coloring[4]);
                 break;
             }
         }
     }
 }
 
-__global__ void kernelResolveConflicts(int numVertices, int numEdges, int blockSize,
-        const int *vertices, const int *neighbors, int *worklist, int *coloring, int *worklistEmpty) {
-    int v = blockIdx.x * blockSize + threadIdx.x;
+/**
+ * @brief Detect conflicts from speculative coloring phase, and remove valid vertices from worklist
+ * @param[in] context All data structures allocated on CUDA device
+ */
+__global__ void kernelResolveConflicts(struct cudaContext context) {
+    int v = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+
+    int numVertices = context.numVertices;
+    const int *vertices = context.vertices;
+    const int *neighbors = context.neighbors;
+    int *worklist = context.worklist;
+    int *coloring = context.coloring;
+    int *worklistEmptyFlag = context.worklistEmptyFlag;
+
     if(v < numVertices && worklist[v]) {
-//        printf("%d worklist empty %d\n", v, *worklistEmpty);
-        // Resolve conflicts and determine the remaining worklist
-        for(int j = vertices[v]; j < vertices[v+1]; j++) {
-            int w = neighbors[j];
+        // Resolve conflicts between neighbors and determine the remaining worklist
+        for(int i = vertices[v]; i < vertices[v+1]; i++) {
+            int w = neighbors[i];
             if((v < w) && (coloring[v] == coloring[w])) {
-//                printf("%d Conflict detected: worklist empty %d\n", v, *worklistEmpty);
-                // Indicate that elements still remain in the worklist
-                *worklistEmpty = 0;
+                // Indicate that worklist is not empty and elements still remain
+                *worklistEmptyFlag = 0;
                 return;
             }
         }
@@ -73,152 +98,68 @@ __global__ void kernelResolveConflicts(int numVertices, int numEdges, int blockS
     }
 }
 
-const int *gmColoring(const Graph &graph) {
+/**
+ * @brief Copy graph representation to CUDA device and allocate all necessary data structures
+ * @param[in] graph The graph to color
+ * @return context A cudaContext struct containing all device data structures
+ */
+struct cudaContext setup(const Graph &graph) {
     int numVertices = graph.getNumVertices();
     int numEdges = graph.getNumEdges();
     const int *vertices = graph.getCSRVertices();
     const int *neighbors = graph.getCSRNeighbors();
 
-    // Initialize memory and copy CSR representation to CUDA device
-    int *cudaVertices = NULL;
-    int *cudaNeighbors = NULL;
-    int *cudaWorklist = NULL;
-    int *cudaColoring = NULL;
-    int *cudaWorklistEmpty = NULL;
-
-    cudaMalloc(&cudaVertices, sizeof(int) * (numVertices + 1));
-    cudaMalloc(&cudaNeighbors, sizeof(int) * 2 * numEdges);
-    cudaMalloc(&cudaWorklist, sizeof(int) * numVertices);
-    cudaMalloc(&cudaColoring, sizeof(int) * numVertices);
-    cudaMalloc(&cudaWorklistEmpty, sizeof(int));
+    // Initialize data structures
+    struct cudaContext context;
+    context.numVertices = numVertices;
+    context.numEdges = numEdges;
+    cudaMalloc(&context.vertices, sizeof(int) * (numVertices + 1));
+    cudaMalloc(&context.neighbors, sizeof(int) * 2 * numEdges);
+    cudaMalloc(&context.worklist, sizeof(int) * numVertices);
+    cudaMalloc(&context.coloring, sizeof(int) * numVertices);
+    cudaMalloc(&context.worklistEmptyFlag, sizeof(int));
    
     // Note that cudaMemset sets values per byte, so 0x01 => 0x01010101 = 16843009
-    cudaMemcpy(cudaVertices, vertices, sizeof(int) * (numVertices + 1), cudaMemcpyHostToDevice);
-    cudaMemcpy(cudaNeighbors, neighbors, sizeof(int) * 2 * numEdges, cudaMemcpyHostToDevice);
-    cudaMemset(cudaWorklist, 0x01, sizeof(int) * numVertices);
-    cudaMemset(cudaColoring, 0x00, sizeof(int) * numVertices);
-    cudaMemset(cudaWorklistEmpty, 0x00, sizeof(int));
+    cudaMemcpy(context.vertices, vertices, sizeof(int) * (numVertices + 1), cudaMemcpyHostToDevice);
+    cudaMemcpy(context.neighbors, neighbors, sizeof(int) * 2 * numEdges, cudaMemcpyHostToDevice);
+    cudaMemset(context.worklist, 0x01, sizeof(int) * numVertices);
+    cudaMemset(context.coloring, 0x00, sizeof(int) * numVertices);
+    cudaMemset(context.worklistEmptyFlag, 0x00, sizeof(int));
 
-    int localWorklistEmpty = 0;
+    return context;
+}
 
-    // Kernel size, must ensure that not all computations are running at once in single block
-    int blockSize = 0;
-    int gridSize = 0;
-    if(numVertices < 2 * BLOCK_SIZE) {
-        blockSize = (numVertices + 1) / 2;
-        gridSize = 2;
-    } else {
-        blockSize = BLOCK_SIZE;
-        gridSize = (numVertices + blockSize - 1) / blockSize;
-    }
-    dim3 blockDim(blockSize);
-    dim3 gridDim(gridSize);
+/**
+ * @brief Performs graph coloring using the GM algorithm
+ * @param[in] context A cudaContext sturct containing all device data structures
+ * @return coloring The outputted coloring
+ */
+const int *gmColoring(struct cudaContext context) {
+    int numVertices = context.numVertices;
 
-    int *coloring = new int[numVertices];
+    // Define kernel size and device flags
+    dim3 blockDim(BLOCK_SIZE);
+    dim3 gridDim((numVertices + BLOCK_SIZE - 1) / BLOCK_SIZE);
+    int worklistEmptyFlag = 0;
 
     // Loop until worklist is empty
-    while(true) {
+    while(!worklistEmptyFlag) {
         // Determine which colors are permissible for each vertex
-        kernelSpeculate<<<gridDim, blockDim>>>(numVertices, numEdges, blockSize,
-                cudaVertices, cudaNeighbors, cudaWorklist, cudaColoring);
+        kernelSpeculate<<<gridDim, blockDim>>>(context);
         cudaDeviceSynchronize();
 
         // Resolve conflicts and determine the remaining worklist
-        cudaMemset(cudaWorklistEmpty, 0x01, sizeof(int));
-        kernelResolveConflicts<<<gridDim, blockDim>>>(numVertices, numEdges, blockSize,
-                cudaVertices, cudaNeighbors, cudaWorklist, cudaColoring, cudaWorklistEmpty);
-        cudaMemcpy(&localWorklistEmpty, cudaWorklistEmpty, sizeof(int), cudaMemcpyDeviceToHost);
+        cudaMemset(context.worklistEmptyFlag, 0x01, sizeof(int));
+        kernelResolveConflicts<<<gridDim, blockDim>>>(context);
+        cudaMemcpy(&worklistEmptyFlag, context.worklistEmptyFlag, sizeof(int), cudaMemcpyDeviceToHost);
         cudaDeviceSynchronize();
-
-        cudaMemcpy(coloring, cudaColoring, sizeof(int) * numVertices, cudaMemcpyDeviceToHost);
-        cudaDeviceSynchronize();
-//        printColoring(coloring, numVertices);
-
-        // Abort if worklist is empty
-        if(localWorklistEmpty) {
-            break;
-        }
     }
     
     // Retrieve coloring from device
-//    int *coloring = new int[numVertices];
-//    cudaMemcpy(coloring, cudaColoring, sizeof(int) * numVertices, cudaMemcpyDeviceToHost);
+    int *coloring = new int[numVertices];
+    cudaMemcpy(coloring, context.coloring, sizeof(int) * numVertices, cudaMemcpyDeviceToHost);
     return coloring;
 }
-
-/*
-std::vector<int> gmColoring(const Graph &graph) {
-    int n = graph.getNumVertices();
-    const int *vertices = graph.getCSRVertices();
-    const int *neighbors = graph.getCSRNeighbors();
-
-    // Vertex indexed array, storing color of each vertex
-    std::vector<int> coloring(n);
-
-    // Keep a worklist, the set of vertices that still need to be considered
-    std::vector<int> W(n);
-    for(int i = 0; i < n; i++) {
-        W[i] = i;
-    }
-
-    // Color indexed array for each thread, marking colors that a particular vertex v cannot have
-    std::vector<int> *colorMask = new std::vector<int>[numThreads];
-    for(int tid = 0; tid < numThreads; tid++) {
-        colorMask[tid].resize(n);
-    }
-
-    while(!W.empty()) {
-        // Determine which colors are permissible for each vertex
-        #pragma omp parallel
-        {
-            int tid = 0;
-
-            #pragma omp for
-            for(int i = 0; i < (int)W.size(); i++) {
-                int v = W[i];
-                for(int j = vertices[v]; j < vertices[v+1]; j++) {
-                    int w = neighbors[j];
-                    colorMask[tid][coloring[w]] = v;
-                }
-                for(int i = 0; i < n; i++) {
-                    if(colorMask[tid][i] != v) {
-                        coloring[v] = i;
-                        break;
-                    }
-                }
-            }
-        }
-
-        // Initialize the remaining worklist
-        std::vector<int> R;
-        #pragma omp parallel
-        {
-            std::vector<int> localR;
-
-            #pragma omp for
-            for(int i = 0; i < (int)W.size(); i++) {
-                int v = W[i];
-                for(int j = vertices[v]; j < vertices[v+1]; j++) {
-                    int w = neighbors[j];
-                    if((v < w) && (coloring[v] == coloring[w])) {
-                        localR.push_back(v);
-                    }
-                }
-            }
-
-            #pragma omp critical (joinR)
-            {
-                R.insert(R.end(), localR.begin(), localR.end());
-            }
-        }
-
-        // Update current worklist
-        std::swap(W, R);
-    }
-
-    return coloring;
-}
-*/
 
 int main(int argc, char *argv[]) {
     if(argc < 2) {
@@ -227,9 +168,10 @@ int main(int argc, char *argv[]) {
     }
     const std::string in_filename = argv[1];
     Graph graph = Graph(in_filename);
+    struct cudaContext context = setup(graph);
 
     auto t1 = getTime();
-    const int *coloring = gmColoring(graph);
+    const int *coloring = gmColoring(context);
     auto t2 = getTime();
     std::cout << "Time: " << getMillis(t1, t2) << "ms\n";
 
